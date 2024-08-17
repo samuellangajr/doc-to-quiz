dd7a2c66d939222c570993779bea04e248548eda
samuellangajr <samuellangajr@gmail.com>
2024-08-18 00:31:49 +0200

    feat: make back home work

--- src/components/buttons/backButton.tsx
+++ src/components/buttons/backButton.tsx
@@ -1,11 +1,19 @@
-import Link from "next/link";
 import { TiHome } from "react-icons/ti";
+
 const BackButton: React.FC = () => {
+    const refreshPage = () => {
+        window.location.reload();
+    };
+
     return (
-        <div className="flex items-center space-x-1 border rounded-md shadow-md mb-2 cursor-pointer text-green-500 bg-white border-green-500 p-2 transition-transform duration-300 hover:scale-110">
+        <div
+            onClick={refreshPage}
+            className="flex items-center space-x-1 border rounded-md shadow-md mb-2 cursor-pointer text-green-500 bg-white border-green-500 p-2 transition-transform duration-300 hover:scale-110"
+        >
             <TiHome size={20} />
-            <Link href={"/"}>Back Home</Link>
+            <span>Back Home</span>
         </div>
     );
 };
+
 export default BackButton;
