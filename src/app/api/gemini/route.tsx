79f466727df0e34b4bf4442a302823ff4ce76df0
samuellangajr <samuellangajr@gmail.com>
2024-08-18 22:27:35 +0200

    refact: Giving more details on promt

--- src/app/api/gemini/route.tsx
+++ src/app/api/gemini/route.tsx
@@ -31,8 +31,32 @@ export async function POST(request: NextRequest) {
         const fileBase64 = await handleFile(file);
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-        const prompt =
-            "Make a quiz with one question, 4 options, and the answer in JSON format. Just write the JSON. dont put the json title.";
+        const prompt = `Create a quiz in JSON format with the following structure:
+        - Include exactly one question.
+        - Provide exactly four unique options.
+        - Specify the correct answer by providing the full text of the correct option.
+        
+        The JSON should have the following keys:
+        - "question": A string representing the question.
+        - "options": An array of four unique strings, each representing one of the options.
+        - "answer": A string with the full text of the correct option.
+        
+        Write only the JSON. Do not include any additional text or title.
+        
+        Example:
+        {
+            "question": "What is the capital of France?",
+            "options": [
+                "Berlin",
+                "Madrid",
+                "Paris",
+                "Rome"
+            ],
+            "answer": "Paris"
+        }
+        Always use the language of the file. 
+        `;
+        
         const generatedContent = await model.generateContent([
             prompt,
             fileBase64,
